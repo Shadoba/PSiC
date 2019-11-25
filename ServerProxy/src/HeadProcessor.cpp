32fc8e22141cce3b498699037a59e30efa0d0931
Atloas <miter010@gmail.com>
2019-11-25 11:30:35 +0100

    Reduced HeadProcessor to bare minimum, actual one on it's own branch

--- ServerProxy/src/HeadProcessor.cpp
+++ ServerProxy/src/HeadProcessor.cpp
@@ -5,40 +5,7 @@
 HeadProcessor::HeadProcessor(std::string head)
 {
     m_head = head;
-
-    std::vector<std::string> lines = Utility::splitString(head, '\n');
-
-    std::vector<std::string> firstLineWords = Utility::splitString(lines.at(0), ' ');
-    lines.erase(lines.begin());
-    m_method = firstLineWords.at(0);
-    m_url = firstLineWords.at(1);
-    m_protocol = firstLineWords.at(2);
-
-    for(unsigned int i = 0; i < lines.size(); i++)
-    {
-        std::vector<std::string> header = Utility::splitString(lines.at(i), ": ");
-        m_headers.insert_or_assign(header.at(0), header.at(1));
-    }
-}
-
-std::string HeadProcessor::getMethod()
-{
-    return m_method;
-}
-
-std::string HeadProcessor::getUrl()
-{
-    return m_url;
-}
-
-std::string HeadProcessor::getProtocol()
-{
-    return m_protocol;
-}
-
-std::map<std::string, std::string> HeadProcessor::getHeaders()
-{
-    return m_headers;
+    m_outputHead = head;
 }
 
 std::string HeadProcessor::getOutputHead()
