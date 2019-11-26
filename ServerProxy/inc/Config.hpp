e0eeb85207b980a6a908621b8a8f7f190b332022
Bartlomiej Mucha <5mucha1@taurus.fis.agh.edu.pl>
2019-11-26 23:19:55 +0100

    General docs added

--- ServerProxy/inc/Config.hpp
+++ ServerProxy/inc/Config.hpp
@@ -7,26 +7,54 @@
 
 #define LOGGER Config::getInstance().getLogger()
 
+/**
+ * @brief class Config stores configuration of server
+ * 
+ */
 class Config 
 {
 public:
+    /**
+     * @brief Initializes server, could be called many times
+     *  m_LoggerStream is std::cout by default
+     * 
+     * @param LoggerStream std::ostream & [INOUT] stream to which logs will be inserted
+     */
     void init(std::ostream & LoggerStream)
     {
         m_LoggerStream = &LoggerStream;
     }
+
+    /**
+     * @brief Get the Instance object (singleton)
+     * 
+     * @return Config& Instance object
+     */
     static Config & getInstance()
     {
         static Config instance;
         return instance;
     }
+
+    /**
+     * @brief Get the currently set Log object
+     * 
+     * @return std::ostream& 
+     */
     std::ostream & getLogger()
     {
         SERVER_ASSERT_MSG(nullptr == m_LoggerStream, "Server not configured")
         return *m_LoggerStream;
     }
 private:
-    Config() { m_LoggerStream = nullptr; }
-    std::ostream * m_LoggerStream;
+
+    /**
+     * @brief Construct a new Config object
+     * 
+     */
+    Config() { m_LoggerStream = &std::cout; }
+
+    std::ostream * m_LoggerStream;  //<? pointer to log stream instance
 };
 
 #endif /* __CONFIG__ */
--- ServerProxy/inc/ProxyConnection.hpp
+++ ServerProxy/inc/ProxyConnection.hpp
@@ -3,29 +3,67 @@
 
 #include <string>
 
-
+/**
+ * @brief Class ProxyConnection stores data on 
+ *  connection between two peers
+ */
 class ProxyConnection
 {
 public:
+    /**
+     * @brief Construct a new Proxy Connection object
+     * 
+     * @param clientSocket void* [IN] pointer to seed socket
+     * @param clientId unsigned char * [IN] id of seed
+     * @param serverSocket void* [IN] pointer to peer socket
+     * @param serverId unsigned char * [IN] id of peer
+     * @param secure bool [IN] true if connection is encrypted
+     */
     ProxyConnection(void *clientSocket, unsigned char * clientId, void *serverSocket, unsigned char * serverId, bool secure);
 
+    /**
+     * @brief Get the Client Socket object
+     * 
+     * @return const void* pointer to seed socket
+     */
     const void * getClientSocket() const;
 
+    /**
+     * @brief Get the Client Id object
+     * 
+     * @return const std::basic_string <unsigned char>& id of seed
+     */
     const std::basic_string <unsigned char> & getClientId() const;
 
+    /**
+     * @brief Get the Server Socket object
+     * 
+     * @return const void* peer socket
+     */
     const void * getServerSocket() const;
 
+    /**
+     * @brief Get the Server Id object
+     * 
+     * @return const std::basic_string <unsigned char>& id of peer
+     */
     const std::basic_string <unsigned char> & getServerId() const;
 
+    /**
+     * @brief Get the Secure object
+     * 
+     * @return true connection is encrypted
+     * @return false connection is not encrypted
+     */
     const bool getSecure() const;
 
 
 protected:
-    const void * m_clientSocket;
-    const std::basic_string <unsigned char> m_clientId;
-    const void * m_serverSocket;
-    const std::basic_string <unsigned char> m_serverId;
-    const bool m_secure;
+    const void * m_clientSocket;                        //<? pointer to seed socket
+    const std::basic_string <unsigned char> m_clientId; //<? id of seed
+    const void * m_serverSocket;                        //<? pointer to peer socket
+    const std::basic_string <unsigned char> m_serverId; //<? id of peer
+    const bool m_secure;                                //<? determines encryption
 };
 
 #endif /* __PROXYCONNECTION__ */
