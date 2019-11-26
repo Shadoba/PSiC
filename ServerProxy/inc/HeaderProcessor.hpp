12c2f3aeaee6a80f6b90ae622aa5fd96d0e7735b
Atloas <miter010@gmail.com>
2019-11-26 20:03:35 +0100

    Added URI, changed some names

--- ServerProxy/inc/HeaderProcessor.hpp
+++ ServerProxy/inc/HeaderProcessor.hpp
@@ -3,25 +3,28 @@
 #include <string>
 #include <map>
 
-class HeadProcessor
+class HeaderProcessor
 {
 public:
-    HeadProcessor(std::string head);
+    HeaderProcessor(std::string head);
 
     std::string getMethod();
     std::string getUrl();
+    std::string getUri();
     std::string getProtocol();
-    std::map<std::string, std::string> getHeaders();
+    std::map<std::string, std::string> getFields();
     std::string getOutputHead();
 
 private:
     void pullDataFromHead(std::string head);
     void transformHead(std::string head);
+    void calculateUri();
 
     std::string m_head;
     std::string m_method;
     std::string m_url;
+    std::string m_uri;
     std::string m_protocol;
-    std::map<std::string, std::string> m_headers;
-    std::string m_outputHead;
+    std::map<std::string, std::string> m_fields;
+    std::string m_outputHeader;
 };
---
+++ ServerProxy/src/HeaderProcessor.cpp
@@ -0,0 +1,86 @@
+#include "HeaderProcessor.hpp"
+
+#include "Utility.hpp"
+
+#include <iostream>
+
+HeaderProcessor::HeaderProcessor(std::string head)
+{
+    m_head = head;
+    pullDataFromHead(head);
+    if(m_method.compare(std::string("CONNECT")))        //0 to równe, czyli wchodzi jeśli nie "CONNECT"
+        transformHead(head);
+}
+
+std::string HeaderProcessor::getMethod()
+{
+    return m_method;
+}
+
+std::string HeaderProcessor::getUrl()
+{
+    return m_url;
+}
+
+std::string HeaderProcessor::getProtocol()
+{
+    return m_protocol;
+}
+
+std::map<std::string, std::string> HeaderProcessor::getFields()
+{
+    return m_fields;
+}
+
+std::string HeaderProcessor::getOutputHead()
+{
+    return m_outputHeader;
+}
+
+void HeaderProcessor::pullDataFromHead(std::string head)
+{
+    std::vector<std::string> lines = Utility::splitString(head, "\r\n");
+    std::vector<std::string> firstLineWords = Utility::splitString(lines.at(0), ' ');
+    lines.erase(lines.begin());
+    m_method = firstLineWords.at(0);
+    m_url = firstLineWords.at(1);
+    m_protocol = firstLineWords.at(2);
+
+    calculateUri();
+
+    for(unsigned int i = 0; i < lines.size(); i++)
+    {
+        if(!lines.at(i).empty())
+        {
+            std::vector<std::string> field = Utility::splitString(lines.at(i), ": ");
+            m_fields.insert(std::pair<std::string, std::string>(field.at(0), field.at(1)));
+        }
+    }
+}
+
+//Na razie tylko przepisuje i podmienia URL, jeśli trzeba
+void HeaderProcessor::transformHead(std::string head)
+{
+    m_outputHeader += m_method + " ";
+    m_outputHeader += m_uri + " ";
+    m_outputHeader += m_protocol + "\r\n";
+    for(std::map<std::string, std::string>::iterator it = m_fields.begin(); it != m_fields.end(); it++)
+    {
+        m_outputHeader += it->first + ": " + it->second + "\r\n";
+    }
+}
+
+void HeaderProcessor::calculateUri()
+{
+    size_t position = m_url.find('/');
+    if(position != 0)                                       //Jeśli pierwszym znakiem nie jest '/', to mamy pełen URL
+    {
+        position = m_url.find('/', position + 2);
+        if(position == std::string::npos)                   //Jeśli poza "http://" nie ma '/', to musimy dodać sam '/'
+            m_uri = std::string(" /");
+        else
+            m_uri = std::string(" " + m_url.substr(position));
+    }
+    else
+        m_uri = m_url;
+}
