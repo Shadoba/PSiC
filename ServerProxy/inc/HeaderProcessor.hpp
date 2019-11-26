e90ae8a6965cdf7701c96336d8afa261cc8677c8
Bartlomiej Mucha <5mucha1@taurus.fis.agh.edu.pl>
2019-11-26 23:20:03 +0100

    General docs added

--- ServerProxy/inc/HeaderProcessor.hpp
+++ ServerProxy/inc/HeaderProcessor.hpp
@@ -6,33 +6,73 @@
 #include <string>
 #include <map>
 
+/**
+ * @brief Class HeaderProcessor process' and stores header data
+ * 
+ */
 class HeaderProcessor
 {
 public:
+    /**
+     * @brief Construct a new Header Processor object
+     * 
+     * @param head extracted from datagram header
+     */
     HeaderProcessor(const std::string head);
 
-    const std::string & InputHeader;
-    const std::string & Method;
-    const std::string & Url;
-    const std::string & Uri;
-    const std::string & Protocol;
-    const std::string & OutputHeader;
+    const std::string & InputHeader;    //<? input header handle
+    const std::string & Method;         //<? method handle
+    const std::string & Url;            //<? url handle
+    const std::string & Uri;            //<? uri handle
+    const std::string & Protocol;       //<? protocol handle
+    const std::string & OutputHeader;   //<? output header handle
+    /**
+     * @brief Get the Fields object
+     * 
+     * @return std::map<std::string, std::string> map with all attributes from header
+     */
     std::map<std::string, std::string> getFields() const;
 
+    /**
+     * @brief Get the Http Request Method Enum object
+     * 
+     * @return httpRequest::httpRequestMethod Http Request Method Enum
+     */
     httpRequest::httpRequestMethod getHttpRequestMethodEnum();
+
+    /**
+     * @brief Get the Protocol Enum object
+     * 
+     * @return protocol::protocol Protocol Enum
+     */
     protocol::protocol getProtocolEnum();
 
 private:
+    /**
+     * @brief extract data from provided header and
+     *  fills them the m_fields
+     * @param head provided header
+     */
     void pullDataFromHead(std::string head);
+
+    /**
+     * @brief tarnsforms output header to be ready to bee sent
+     * 
+     * @param head //TODO remove, kind a legacy argument 
+     */
     void transformHead(std::string head);
+
+    /**
+     * @brief Extracts uri from url
+     * 
+     */
     void calculateUri();
 
-    std::string m_head;
-    std::string m_method;
-    std::string m_url;
-    std::string m_uri;
-    std::string m_protocol;
-    std::string m_outputHeader;
-
-    std::map<std::string, std::string> m_fields;
+    std::string m_head;         //<? input header
+    std::string m_method;       //<? method
+    std::string m_url;          //<? url
+    std::string m_uri;          //<? uri
+    std::string m_protocol;     //<? protocol
+    std::string m_outputHeader; //<? output header
+    std::map<std::string, std::string> m_fields;    //<? Attributes of header
 };
--- ServerProxy/inc/serverException.hpp
+++ ServerProxy/inc/serverException.hpp
@@ -7,26 +7,69 @@
 #define SERVER_ASSERT(cond) serverException::assert(cond, "", __func__, errno, __LINE__, __FILE__);
 #define SERVER_ASSERT_MSG(cond, msg) serverException::assert(cond, msg, __func__, errno, __LINE__, __FILE__);
 
+/**
+ * @brief Excpetion
+ * 
+ */
 class serverException : public std::exception
 {
-public: 
-    virtual const char* what() const throw();
-    virtual int getErrno() const;
+public:
+    /**
+     * @brief reurns message of what happened
+     * 
+     * @return const char* error message
+     */
+    const char* what() const throw() override;
 
+    /**
+     * @brief Get the Errno id
+     * 
+     * @return int errno id
+     */
+    int getErrno() const;
+
+    /**
+     * @brief Test if requiremnts in condition are met
+     * 
+     * @param condition bool [IN] condition
+     * @param msg cstring [IN] user message
+     * @param func cstring [IN] being tested function's name
+     * @param err int [IN] unix error id
+     * @param line int [IN] line in file
+     * @param file cstring [IN] filename where test takes place
+     */
     static void assert(bool condition, const char * const msg, const char * const func, int err, int line, const char * const file);
 
+    /**
+     * @brief Destroy the server Exception object
+     * 
+     */
     ~serverException();
 
 protected:
-	serverException(const char * const msg, const char * const func, int err, int line, const char * const file);
-	serverException() = delete;
-	
+    /**
+     * @brief Construct a new server Exception object
+     * 
+     * @param msg cstring [IN] user message
+     * @param func cstring [IN] being tested function's name
+     * @param err int [IN] unix error id
+     * @param line int [IN] line in file
+     * @param file cstring [IN] filename where test takes place
+     */
+    serverException(const char * const msg, const char * const func, int err, int line, const char * const file);
+
+    /**
+     * @brief Deleted default constructor
+     * 
+     */
+    serverException() = delete;
+
 private:
-	const int m_Errno;
-    std::string m_Message;
-    const int m_line;
-    std::string m_FunctionName;
-    std::string m_file;
+    const int m_Errno;          //<? unix error id
+    std::string m_Message;      //<? user message
+    const int m_line;           //<? line in file
+    std::string m_FunctionName; //<? being tested function's name
+    std::string m_file;         //<? filename where test takes place
 };
 
 #endif /* __SERVEREXCEPTION__ */
