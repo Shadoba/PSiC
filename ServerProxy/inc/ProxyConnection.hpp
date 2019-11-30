da7d08cb3306c4676a93c207bbb4cf2d166aa8de
Atloas <miter010@gmail.com>
2019-11-30 20:36:46 +0100

    Fix merge oversight

--- ServerProxy/inc/ProxyConnection.hpp
+++ ServerProxy/inc/ProxyConnection.hpp
@@ -35,8 +35,8 @@ public:
      */
     const bool getSecure() const;
 
-    const std::basic_string <unsigned char> m_clientId; //<? id of seed
-    const std::basic_string <unsigned char> m_serverId; //<? id of peer
+    const std::string m_clientId; //<? id of seed
+    const std::string m_serverId; //<? id of peer
     const bool m_secure;                                //<? determines encryption
 };
 
